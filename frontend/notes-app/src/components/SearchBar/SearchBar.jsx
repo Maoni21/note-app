0fad73d89bd8f9a11e51e7f13bf91305a5e3c188
Imrane Mesbahi <mesbahi.imrane@gmail.com>
2024-07-27 00:13:33 +0200

    Home Commit

---
+++ frontend/notes-app/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaMagnifyingGlass } from 'react-icons/fa6'
+import { IoMdClose } from 'react-icons/io'
+
+const SearchBar = ({value, onChange, handleSearch, onClearSearch}) => {
+  return (
+    <div className='w-80 flex items-center px-4 bg-slate-100 rounded-md'>
+        <input 
+            type="text"
+            placeholder='Rechercher vos notes'
+            className='w-full text-xs bg-transparent py-[11px] outline-none'
+            value={value}
+            onChange={onChange} 
+        />
+
+            <FaMagnifyingGlass className='' onClick={handleSearch} />
+
+    </div>
+  )
+}
+
+export default SearchBar
