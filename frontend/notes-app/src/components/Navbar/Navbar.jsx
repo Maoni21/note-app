cb74b372ecfa00ddc6f7cb9bdede79e71c723482
Imrane Mesbahi <mesbahi.imrane@gmail.com>
2024-07-27 00:44:48 +0200

    Search Commit

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ProfileInfo from '../Cards/ProfileInfo'
 import { useNavigate } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
 
 const Navbar = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
 
   const navigate = useNavigate
 
@@ -11,12 +14,29 @@ const Navbar = () => {
     navigate("/login")
   }
 
+  const handleSearch = () => {
+
+  }
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  }
+
+
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         
         <h2 className='text-xl font medium text-black py-2'>Notes</h2>
 
-        <SearchBar />
+        <SearchBar 
+          value={searchQuery}
+          onChange={({ target }) => {
+            setSearchQuery(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        
+        />
 
         <ProfileInfo onLogout={onLogout}/>
     </div>
--- frontend/notes-app/src/components/SearchBar/SearchBar.jsx
+++ frontend/notes-app/src/components/SearchBar/SearchBar.jsx
@@ -13,7 +13,15 @@ const SearchBar = ({value, onChange, handleSearch, onClearSearch}) => {
             onChange={onChange} 
         />
 
-            <FaMagnifyingGlass className='' onClick={handleSearch} />
+        {value && ( 
+          <IoMdClose 
+            className='text-xl text-slate-500 cursor-pointer hover:text-black mr-3' 
+            onClick={onClearSearch} 
+          />
+        )}
+
+
+            <FaMagnifyingGlass className='text-slate-400 cursor-pointer hover:text-black' onClick={handleSearch} />
 
     </div>
   )
